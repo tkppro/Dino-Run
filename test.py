3083593fc3cafec2fea82cef85923d9d8102b55a
tkppro <tkpproo@gmail.com>
2019-12-02 09:48:56 +0700

    update new code

--- test.py
+++ test.py
@@ -1,51 +1,74 @@
-
-# import the necessary packages
-from __future__ import print_function
-from imutils.video import WebcamVideoStream
-from imutils.video import FPS
 import argparse
-import imutils
-from VideoShow import *
-from HandDetection import *
 import cv2
-# created a *threaded* video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from webcam...")
+import pyautogui
+import copy
+import numpy as np
+from VideoGet import VideoGet
+from VideoShow import VideoShow
+from FingerDetection import *
+from KeyListener import *
 
-cap = cv2.VideoCapture(0)
-(grabbed, frame) = cap.read()
-video_shower = VideoShow(frame).start()
-hand_detection = HandDetection(frame).start()
+x0, y0, width = 300, 180, 300
+x1, y1 = 0, 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+fx, fy, fh = 10, 50, 45
 
-width = 1366
-height = 500
+def handDetect(frame, prediction, x, y):
+    """
+    Add iterations per second text to lower-left corner of a frame.
+    """
+    cv2.putText(frame, '%s' % (prediction), (x+10,y+50), font, 1.0, (245, 210, 65), 2, 1)
+    return prediction
 
-width1 = 500
-height2 = 500
-# cps = CountsPerSec().start()
 
-# loop over some frames...this time using the threaded stream
-while True:
-        (grabbed, frame) = cap.read()
-        if not grabbed or video_shower.stopped or hand_detection.stopped:
+
+def binaryMask(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.GaussianBlur(img, (7,7), 3)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    ret, new = cv2.threshold(img, 25, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    return new
+
+def normalizeFrame(frame, x, y):
+
+    roi = frame[y:y + width, x:x + width]
+    roi = binaryMask(roi)
+
+    img = np.float32(roi) / 255.
+    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=-1)
+
+    return img
+
+def drawDetectionArea(frame, x, y, width, color):
+    cv2.rectangle(frame, (x, y), (x + width - 1, y + width - 1), color, 12)
+    return frame
+
+def main():
+    finger_detect = FingerDetection().start()
+    key_listener = KeyListener().start()
+    video_getter = VideoGet(0).start()
+    video_shower = VideoShow(video_getter.frame).start()
+
+    while True:
+        if video_getter.stopped or video_shower.stopped:
             video_shower.stop()
-            hand_detection.stop()
+            video_getter.stop()
             break
-        frame = imutils.resize(frame, width=1500)
+
+        frame = video_getter.frame
         frame = cv2.flip(frame, 1)
-        cv2.rectangle(frame, (866, 0), (width, height), (0, 255, 0), 5)
-        cv2.rectangle(frame, (0, 0), (width1, height2), (0, 0, 255), 5)
-        crop_image = frame[0:height, 866:width]
+        img = normalizeFrame(frame, x0, y0)
+        frame = drawDetectionArea(frame,x0,y0,width,(0,255,0))
 
-        # frame = putIterationsPerSec(frame, cps.countsPerSec())
+        finger_detect.frame = img
+        prediction = handDetect(frame, finger_detect.detect(),x0,y0)
+
+        # if prediction == "NONE" or prediction == "FIVE":
+        key_listener.prediction = prediction
+        key_listener.handle('up', 'down')
+
         video_shower.frame = frame
-        hand_detection.crop_image = crop_image
 
-        # cps.increment()
-
-# stop the timer and display FPS information
-
-
-# do a bit of cleanup
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
